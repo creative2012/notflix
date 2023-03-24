0c3a1c904fc26708866c576c136299b5e3cf6b9a
Paul Morris <creative2012@hotmail.com>
2023-03-24 22:20:23 +0000

    added react router as dependancy to register and login to register, also router.push on login success

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,8 +2,11 @@ import Input from "@/components/input";
 import { useCallback, useState } from "react";
 import axios from "axios";
 import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router";
 
 const Auth = () => {
+    const router = useRouter();
+
     const [userName, setUserName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -12,20 +15,7 @@ const Auth = () => {
 
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
-    }, [])
-
-    const register = useCallback(async () => {
-        try {
-            await axios.post('api/register', {
-                email,
-                userName,
-                password
-            });
-
-        } catch (error) {
-            console.log(error)
-        }
-    }, [email, userName, password]);
+    }, []);
 
     const login = useCallback(async () => {
         try {
@@ -35,11 +25,28 @@ const Auth = () => {
                 redirect: false,
                 callbackUrl: '/',
             });
+
+            router.push('/');
         } catch (error) {
             console.log(error)
         }
 
-    }, [email, password])
+    }, [email, password, router])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('api/register', {
+                email,
+                userName,
+                password
+            });
+
+            login();
+        } catch (error) {
+            console.log(error)
+        }
+    }, [email, userName, password, login]);
+
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cener bg-fixed bg-cover">
