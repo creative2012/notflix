9b74dcbf4b0a0e43843abee0ce136bcc4bccad96
Paul Morris <creative2012@hotmail.com>
2023-03-25 16:05:45 +0000

    added page protection

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-import { CLIENT_RENEG_LIMIT } from "tls";
+import { PrismaClient } from '@prisma/client'
 
-const client = global.prismadb || new PrismaClient();
-if(process.env.NODE_ENV === "production") global.prismadb = CLIENT_RENEG_LIMIT
+const client = global.prismadb || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') global.prismadb = client
 
-export default client;
+export default client
