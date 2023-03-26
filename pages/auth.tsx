daeb8f28306af8c315d9cfff7942af271726171b
Paul Morris <creative2012@hotmail.com>
2023-03-26 17:04:51 +0100

    added info modal

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,4 @@
-import Input from "@/components/input";
+import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import axios from "axios";
 import { signIn, getSession } from "next-auth/react";
